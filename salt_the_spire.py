2d30de95a5014cb621e061a9defce707da945c20
jessijzhao <jessijzhao@gmail.com>
2022-06-17 00:03:01 -0700

    Ensure consistent style
    * Ensure consistent formatting + docstrings
    * Add type hints to all functions

--- salt_the_spire.py
+++ salt_the_spire.py
@@ -7,51 +7,60 @@ Date: November 29, 2020
 Decodes or encodes save files for Slay the Spire.
 """
 
-import base64
 import argparse
+import base64
 from itertools import cycle
 
 
-def xor_key(bstring, key=b'key'):
+def xor_key(bstring: bytes, key: bytes = b"key") -> bytes:
     """
-    bstring: bytestring to xor with cyclic key
-    key: key phrase (actually just "key")
+    Args:
+        bstring: bytestring to xor with cyclic key
+        key: key phrase (actually just "key")
 
-    Takes the XOR of the input text and the key phrase ("key").
+    Returns the XOR of the input text and the key phrase ("key").
     """
     return bytes(_a ^ _b for _a, _b in zip(bstring, cycle(key)))
 
 
-def decode(inbytes):
+def decode(inbytes: bytes) -> bytes:
     """
-    inbytes: bytes to decode
+    Args:
+        inbytes: bytes to decode
 
     Returns decoded bytes representing a human-readable JSON.
     """
     return xor_key(base64.b64decode(inbytes))
 
 
-def encode(inbytes):
+def encode(inbytes: bytes) -> bytes:
     """
-    inbytes: bytes to encode
+    Args:
+        inbytes: bytes to encode
 
     Returns encoded bytes in Slay the Spire save file format.
     """
     return base64.b64encode(xor_key(inbytes))
 
 
-def main():
+def main() -> None:
 
     parser = argparse.ArgumentParser()
 
     # whether to decode or encode save file
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-d', '--decode', action='store_true', help='decode save file')
-    group.add_argument('-e', '--encode', action='store_true', help='encode save file')
+    group.add_argument("-d", "--decode", action="store_true", help="decode save file")
+    group.add_argument("-e", "--encode", action="store_true", help="encode save file")
 
     # which files to read from / write to
-    parser.add_argument('input', type=argparse.FileType('rb'), help='save file to decode/encode')
-    parser.add_argument('output', type=argparse.FileType('wb'), help='file to store decoded/encoded save file')
+    parser.add_argument(
+        "input", type=argparse.FileType("rb"), help="save file to decode/encode"
+    )
+    parser.add_argument(
+        "output",
+        type=argparse.FileType("wb"),
+        help="file to store decoded/encoded save file",
+    )
 
     args = parser.parse_args()
 
@@ -69,6 +78,5 @@ def main():
     args.output.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
